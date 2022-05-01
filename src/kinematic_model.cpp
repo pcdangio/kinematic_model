5e5728c02fbef81302b403fd9d313870de79aa8f
Paul D'Angio <pcdangio@gmail.com>
2022-05-01 18:38:38 -0400

    kinematic_model: removed ros objects/code

--- src/kinematic_model.cpp
+++ src/kinematic_model.cpp
@@ -1,69 +1,14 @@
 #include <kinematic_model/kinematic_model.hpp>
 
-#include <dlfcn.h>
-
 using namespace kinematic_model;
 
 // CONSTRUCTORS
 kinematic_model_t::kinematic_model_t(uint32_t n_state_variables, uint32_t n_sensors)
     : ukf_t(n_state_variables, n_sensors)
-{
-    // Set up private node handle.
-    kinematic_model_t::m_node = std::make_unique<ros::NodeHandle>("~");
+{}
 
-    // Initialize delta time.
-    kinematic_model_t::dt = 0; 
-
-    // Set up services.
-    kinematic_model_t::m_service_get_transform = kinematic_model_t::m_node->advertiseService("get_transform", &kinematic_model_t::service_get_transform, this);
-}
-std::shared_ptr<kinematic_model_t> kinematic_model_t::load_plugin(const std::string& plugin_path)
-{
-    // Check that path was provided (dl gets handle to program if empty)
-    if(plugin_path.empty())
-    {
-        ROS_ERROR("attempted to load plugin with empty path");
-        return nullptr;
-    }
-
-    // Open plugin shared object library.
-    void* so_handle = dlopen(plugin_path.c_str(), RTLD_NOW);
-    if(!so_handle)
-    {
-        ROS_ERROR_STREAM("failed to load model plugin (" << dlerror() << ")");
-        return nullptr;
-    }
-
-    // Get a reference to the instantiate symbol.
-    typedef kinematic_model_t* (*instantiate_t)();
-    instantiate_t instantiate = reinterpret_cast<instantiate_t>(dlsym(so_handle, "instantiate"));
-    if(!instantiate)
-    {
-        ROS_ERROR_STREAM("failed to load model plugin (" << dlerror() << ")");
-        dlclose(so_handle);
-        return nullptr;
-    }
-
-    // Try to instantiate the plugin.
-    kinematic_model_t* plugin = nullptr;
-    try
-    {
-        plugin = instantiate();
-    }
-    catch(const std::exception& error)
-    {
-        ROS_ERROR_STREAM("failed to instantiate model plugin (" << error.what() << ")");
-        dlclose(so_handle);
-        return nullptr;
-    }
-
-    // Return the plugin as a shared ptr with a custom deleter.
-    return std::shared_ptr<kinematic_model_t>(plugin,
-                                           [so_handle](kinematic_model_t* plugin){delete plugin; dlclose(so_handle);});
-}
-
-// METHODS
-bool kinematic_model_t::initialize()
+// OPERATION
+void kinematic_model_t::initialize()
 {
     // First attempt to build geometry design.
     geometry::design_t design;
@@ -73,32 +18,29 @@ bool kinematic_model_t::initialize()
     }
     catch(const std::exception& error)
     {
-        ROS_ERROR_STREAM("plugin::build_geometry failed (" << error.what() << ")");
-        return false;
+        throw std::runtime_error("failed to build geometry (" + std::string(error.what()) + ")");
     }
 
     // Build graph.
     kinematic_model_t::m_graph.build(design);
-
-    return true;
 }
-void kinematic_model_t::run()
+void kinematic_model_t::iterate()
 {
-    // Create loop timer for state estimator.
-    auto p_loop_rate = kinematic_model_t::m_node->param<double_t>("state_estimation_rate", 100);
-    auto loop_timer = kinematic_model_t::m_node->createTimer(ros::Duration(1.0/p_loop_rate), &kinematic_model_t::timer_state_estimation, this);
-    // Initialize state estimation timestamp.
-    kinematic_model_t::m_timestamp_state_estimation = ros::Time::now();
+    // Iterate the underlying UKF.
+    try
+    {
+        ukf_t::iterate();
+    }
+    catch(const std::exception& error)
+    {
+        throw std::runtime_error("state estimation failed (" + std::string(error.what()) + ")");
+    }
 
-    // Spin node.
-    ros::spin();
+    // Reset the transform cache.
+    kinematic_model_t::m_transform_cache.clear();
 }
 
-// PROTECTED METHODS
-void kinematic_model_t::on_state_update()
-{
-    // Do nothing; this is an optional base class space holder.
-}
+// TRANSFORM
 bool kinematic_model_t::get_transform(const std::string& source_frame, const std::string& target_frame, const Eigen::VectorXd& state_vector, transform::transform_t& transform) const
 {
     // Get the transform path from the graph.
@@ -124,38 +66,12 @@ bool kinematic_model_t::get_transform(const std::string& source_frame, const std
         }
 
         // Chain the transform.
-        attachment_transform.transform(transform);
+        attachment_transform.apply(transform);
     }
 
     return true;
 }
-
-// ROS CALLBACKS
-void kinematic_model_t::timer_state_estimation(const ros::TimerEvent& event)
-{
-    // Calculate current delta time for this iteration.
-    auto current_timestamp = ros::Time::now();
-    kinematic_model_t::dt = (current_timestamp - kinematic_model_t::m_timestamp_state_estimation).toSec();
-    kinematic_model_t::m_timestamp_state_estimation = current_timestamp;
-
-    // Run iteration for state estimator.
-    try
-    {
-        kinematic_model_t::iterate();
-    }
-    catch(const std::exception& error)
-    {
-        ROS_FATAL_STREAM("state estimator failed (" << error.what() << ")");
-        ros::shutdown();
-    }
-
-    // Reset the transform cache.
-    kinematic_model_t::m_transform_cache.clear();
-
-    // Call user code.
-    on_state_update();
-}
-bool kinematic_model_t::service_get_transform(geometry_msgs_ext::get_transformRequest& request, geometry_msgs_ext::get_transformResponse& response)
+bool kinematic_model_t::get_transform(const std::string& source_frame, const std::string& target_frame, transform::transform_t& transform)
 {
     // Create an output transform (defaults to identity).
     transform::transform_t transform;
@@ -164,7 +80,7 @@ bool kinematic_model_t::service_get_transform(geometry_msgs_ext::get_transformRe
     bool success = false;
 
     // Check the transform cache.
-    std::string cache_key = request.source_frame + ":" + request.target_frame;
+    std::string cache_key = source_frame + ":" + target_frame;
     auto cache_iterator = kinematic_model_t::m_transform_cache.find(cache_key);
     if(cache_iterator != kinematic_model_t::m_transform_cache.end())
     {
@@ -181,7 +97,7 @@ bool kinematic_model_t::service_get_transform(geometry_msgs_ext::get_transformRe
         // Transform not found in cache.
 
         // Calculate the transform with current state.
-        if(kinematic_model_t::get_transform(request.source_frame, request.target_frame, kinematic_model_t::x, transform))
+        if(kinematic_model_t::get_transform(source_frame, target_frame, kinematic_model_t::x, transform))
         {
             // Transform successfully calculated.
 
@@ -191,14 +107,11 @@ bool kinematic_model_t::service_get_transform(geometry_msgs_ext::get_transformRe
             success = true;
 
             // Add transform forward/reverse to cache.
-            std::string cache_key_reverse = request.target_frame + ":" + request.source_frame;
+            std::string cache_key_reverse = target_frame + ":" + source_frame;
             kinematic_model_t::m_transform_cache[cache_key] = transform;
             kinematic_model_t::m_transform_cache[cache_key_reverse] = transform.inverse();
         }
     }
-
-    // Set up response.
-    response.transform = transform.to_message();
 
     // Indicate if transform calculation was a success.
     return success;
